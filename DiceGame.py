adee833a3963d516a9bcbe1510de651604b36506
s1250123 <s1250123@std6dc39.u-aizu.ac.jp>
2018-07-05 19:20:26 +0900

    Won/lost message modified

--- DiceGame.py
+++ DiceGame.py
@@ -4,7 +4,18 @@ r = Random()
 x = r.randint(1, 6)
 y = r.randint(1, 6)
 
+print'what is your name?'
+s = raw_input('> ')
+print'Hello,', s, '!'
+
 print'Rolling the dice...'
 print'Die 1:', x
 print'Die 2:', y
-print'Total value: ', x+y
+print'Total value:', x+y
+
+
+if x+y >= 8:
+    print s,'won!'
+else:
+    print s,'lost!'
+    
